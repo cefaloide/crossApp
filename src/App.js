77059b2e7a0a08855a49cc3ad22ed9bd7d3bd952
Joan Rueda <joanruedapau@gmail.com>
2022-05-03 00:03:34 +0200

    feat: add reset btn and save localStorage reps

--- src/App.js
+++ src/App.js
@@ -3,36 +3,58 @@ import { useEffect, useMemo, useState } from "react";
 import NoSleep from "nosleep.js";
 
 function App() {
-  const [rep, setRep] = useState(0);
+  const initialRep = localStorage.getItem("rep");
+  const [rep, setRep] = useState(parseInt(initialRep) || 0);
   const noSleep = useMemo(() => new NoSleep(), []);
 
   const plus = () => {
-    console.log("rep: ", rep);
     const newRep = rep + 1;
     setRep(newRep);
+  };
+
+  const reset = () => {
+    setRep(0);
   };
 
   useEffect(() => {
     noSleep.enable();
   }, []);
 
+  useEffect(() => {
+    localStorage.setItem("rep", rep);
+  }, [rep]);
+
   return (
-    <div
-      onClick={plus}
-      style={{
-        background: "lightblue",
-        height: "100vh",
-        fontSize: "20vh",
-        justifyContent: "center",
-        display: "flex",
-        alignItems: "center",
-        MozUserSelect: "none",
-        WebkitUserSelect: "none",
-        msUserSelect: "none",
-      }}
-    >
-      {rep}
-    </div>
+    <>
+      <div
+        onClick={plus}
+        style={{
+          background: "lightblue",
+          height: "100vh",
+          fontSize: "20vh",
+          justifyContent: "center",
+          display: "flex",
+          alignItems: "center",
+          MozUserSelect: "none",
+          WebkitUserSelect: "none",
+          msUserSelect: "none",
+        }}
+      >
+        {rep}
+      </div>
+      <div
+        style={{
+          border: "2px solid cornflowerblue",
+          borderRadius: "5px",
+          display: "inline-block",
+          padding: "10px",
+          background: "coral",
+        }}
+        onClick={reset}
+      >
+        Reset
+      </div>
+    </>
   );
 }
 
