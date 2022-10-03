26b35c3b888893f91d239082bf201274cd36b17e
Joan Rueda <joanruedapau@gmail.com>
2022-10-03 20:44:04 +0200

    test: add click number and reset tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+import NoSleep from "nosleep.js";
+
+jest.spyOn(NoSleep.prototype, "enable").mockImplementation(jest.fn());
+
+beforeEach(() => {
+  window.localStorage.clear();
+});
+
+test("renders number 0 on load", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const number = screen.getByText(/0/i);
+  expect(number).toBeInTheDocument();
+});
+
+test("renders number 1 on click the screen", () => {
+  render(<App />);
+  const number = screen.getByText(/0/i);
+  fireEvent.click(number);
+
+  const newNumber = screen.getByText(/1/i);
+  expect(newNumber).toBeInTheDocument();
+});
+
+test("renders number 3 on click 3 times the screen", () => {
+  render(<App />);
+  const number = screen.getByText(/0/i);
+  fireEvent.click(number);
+  fireEvent.click(number);
+  fireEvent.click(number);
+
+  const newNumber = screen.getByText(/3/i);
+  expect(newNumber).toBeInTheDocument();
+});
+
+test("renders number 0 on click reset button", () => {
+  render(<App />);
+  const number = screen.getByText(/0/i);
+  fireEvent.click(number);
+  fireEvent.click(number);
+  expect(screen.getByText(/2/i)).toBeInTheDocument();
+
+  const resetButton = screen.getByText(/reset/i);
+  fireEvent.click(resetButton);
+  expect(screen.getByText(/0/i)).toBeInTheDocument();
 });
