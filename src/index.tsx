e67df1eaa05cd77b612999b1768a5a949b93711f
Joan Rueda <joanruedapau@gmail.com>
2023-01-23 10:30:47 +0100

    feat: add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 // import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://6cce5623d51047b794a9c44ce196ea22@o4504542373085184.ingest.sentry.io/4504542384947200",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
